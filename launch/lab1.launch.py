a1eca1e6b8e22db2f652aa0a49781d9e702b1768
przemyslaw <01149436@pw.edu.pl>
2021-03-20 18:04:54 +0100

    launch file

---
+++ launch/lab1.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        Node(
+            package='LAB1',
+            prefix='gnome-terminal --',
+            executable='node',
+            name='node_name',
+            output='screen',
+
+
+        )
+ 
+    ])
